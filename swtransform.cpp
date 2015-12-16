8960baccafbf741f2def9189e19f989377afe3e6
artuntun <arranz.91@gmail.com>
2015-12-16 03:20:17 +0100

    Updasted swttransform.cpp

--- swtransform.cpp
+++ swtransform.cpp
@@ -10,16 +10,20 @@
 
 #define PI 3.14159265
 
+
 using namespace cv;
 
 /// Global variables
-Mat gradient_x, gradient_y;
+Mat gradient_x, abs_grad_x, gradient_y, abs_grad_y;
+Mat grad;
 
 int strokeMedianFilter(Mat *SWTimage, std::vector<std::vector<Point2d>> *saved_rays);
 int strokeWidth(int i, int j, short g_x, short g_y, Mat *SWTimage, std::vector<std::vector<Point2d>> *saved_rays, Mat *edge_image);
 int getSteps(int g_x, int g_y, float& step_x, float& step_y);
 Mat SWTransform(Mat* edge_image);
 std::vector<Component> legallyConnectedComponents(Mat* SWTimage);
+std::vector<Component> filterComponents(Mat * SWTimage, std::vector<Component> components);
+std::vector<std::vector<Component>> connectedPairs(std::vector<Component> valid_components);
 
 /** @function main */
 
@@ -45,40 +49,205 @@ int main(int argc, char** argv)
 	Mat detected_edges;
 	Canny(filtered, detected_edges, 100, 300, 3);
 	//gradients
+	Mat gradxx;
 	Sobel(filtered, gradient_x, CV_16S, 1, 0, 3, 1, 0, BORDER_DEFAULT);
+	gradient_x.copyTo(gradxx);
+	//Scharr(filtered, gradient_x, CV_16S, 1, 0, 3);
+	convertScaleAbs(gradient_x, abs_grad_x);
 	Sobel(filtered, gradient_y, CV_16S, 0, 1, 3, 1, 0, BORDER_DEFAULT);
+	//Scharr(filtered, gradient_y, CV_16S, 0, 1, 3);
+	convertScaleAbs(gradient_y, abs_grad_y);
+	/// Total Gradient (approximate)
+	addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad);
 
 	Mat SWTimage = SWTransform(&detected_edges);
 
+
 	std::vector < Component > components;
 	components = legallyConnectedComponents(&SWTimage);
+	std::vector < Component > valid_components;
+	valid_components = filterComponents(&SWTimage, components);
+	std::vector<std::vector<Component>> words;
+	words = connectedPairs(valid_components);
 
-	Mat components_image = cv::Mat(src.rows, src.cols, CV_8UC3, cv::Scalar(0,0,0));
+	Mat components_image = cv::Mat(src.rows, src.cols, CV_8UC3, cv::Scalar(0, 0, 0));
 
-	int color = 0;
-	for (std::vector<Component>::iterator it1 = components.begin(); it1 != components.end(); ++it1){
-		if (color == 3)
-			color = 0;
-		for (std::vector<Point2d>::iterator it2 = it1->points.begin(); it2 != it1->points.end(); ++it2){
-			components_image.at<cv::Vec3b>(it2->y, it2->x)[color] = 255;
+	int count = 0;
+	for (std::vector<std::vector<Component>>::iterator it1 = words.begin(); it1 != words.end(); ++it1){
+		for (std::vector<Component>::iterator it2 = it1->begin(); it2 != it1->end(); ++it2){
+			int color0 = rand() % 256;
+			int color1 = rand() % 256;
+			int color2 = rand() % 256;
+			for (std::vector<Point2d>::iterator it3 = it2->points.begin(); it3 != it2->points.end(); ++it3){
+				components_image.at<cv::Vec3b>(it3->y, it3->x)[0] = color0;
+				components_image.at<cv::Vec3b>(it3->y, it3->x)[1] = color1;
+				components_image.at<cv::Vec3b>(it3->y, it3->x)[2] = color2;
+			}
+			count++;
 		}
-		color++;
 	}
+	std::cout << "Components after filtering" << count << std::endl;
+
+	////////////////////////////////////Rotate certain angle componenents///////////////////////////////////////
+	/*for (std::vector<Component>::iterator it1 = valid_components.begin(); it1 != valid_components.end(); ++it1){
+		int color0 = rand() % 256;
+		int color1 = rand() % 256;
+		int color2 = rand() % 256;
+		std::vector<Point2d> rotated_component = it1->rotate_component(270 * PI / 180);
+		for (std::vector<Point2d>::iterator it2 = rotated_component.begin(); it2 != rotated_component.end(); ++it2){
+			if (it2->x >= 0 && it2->x < components_image.cols && it2->y >= 0 && it2->y < components_image.rows){
+				components_image.at<cv::Vec3b>(it2->y, it2->x)[0] = color0;
+				components_image.at<cv::Vec3b>(it2->y, it2->x)[1] = color1;
+				components_image.at<cv::Vec3b>(it2->y, it2->x)[2] = color2;
+			}
+		}
+	}*/
+	
+	////////////////////////////////////Before filtering components//////////////
+	//count = 0; 
+	//Mat components_image_before = cv::Mat(src.rows, src.cols, CV_8UC3, cv::Scalar(0, 0, 0));
+	//for (std::vector<Component>::iterator it1 = components.begin(); it1 != components.end(); ++it1){
+	//	int color0 = rand() % 256;
+	//	int color1 = rand() % 256;
+	//	int color2 = rand() % 256;
+	//	for (std::vector<Point2d>::iterator it2 = it1->points.begin(); it2 != it1->points.end(); ++it2){
+	//		components_image_before.at<cv::Vec3b>(it2->y, it2->x)[0] = color0;
+	//		components_image_before.at<cv::Vec3b>(it2->y, it2->x)[1] = color1;
+	//		components_image_before.at<cv::Vec3b>(it2->y, it2->x)[2] = color2;
+	//	}
+	//	count++;
+	//}
+	//std::cout << "Components before filtering" << count << std::endl;
+
+	
+
+	////////////////////////Prettier SWTimage/////////////////
+	//SWTimage.convertTo(SWTimage, CV_8U, 1.0);
+	//uchar *ptr4;
+	//for (int i = 0; i < SWTimage.rows; i++){
+	//	ptr4 = SWTimage.ptr<uchar>(i);
+	//	for (int j = 0; j < SWTimage.cols; j++){
+	//		if (ptr4[j] == 0){
+	//			ptr4[j] = 255;
+	//		}
+	//		else
+	//			ptr4[j] = (int)(ptr4[j] * 3 );
+	//	}
+	//}
+
+	///////////////////Boxes over original image////////////////
+	//for (std::vector<Component>::iterator it = valid_components.begin(); it != valid_components.end(); it++){
+	//	Point vertex1;
+	//	vertex1.x = it->minx;
+	//	vertex1.y = it->miny;
+	//	Point vertex2;
+	//	vertex2.x = it->maxx;
+	//	vertex2.y = it->maxy;
+	//	rectangle(src, vertex1, vertex2, Scalar(0, 0, 255));
+	//}
+
+	//Create windo canvas to show img
+	namedWindow("original", CV_WINDOW_AUTOSIZE);
+	namedWindow("gray", CV_WINDOW_AUTOSIZE);
+	namedWindow("filtered", CV_WINDOW_AUTOSIZE);
+	namedWindow("edges", CV_WINDOW_AUTOSIZE);
+	namedWindow("gradientx", CV_WINDOW_AUTOSIZE);
+	namedWindow("gradienty", CV_WINDOW_AUTOSIZE);
+	namedWindow("grad", CV_WINDOW_AUTOSIZE);
+	namedWindow("SWT", CV_WINDOW_AUTOSIZE);
+	namedWindow("connected_components", CV_WINDOW_AUTOSIZE);
+	//Show image in the name of the window
+	imshow("original", src);
+	imshow("gray", src_gray);
+	imshow("filtered", filtered);
+	imshow("edges", detected_edges);
+	imshow("gradientx", abs_grad_x);
+	imshow("gradienty", abs_grad_y);
+	imshow("grad", grad);
+	imshow("SWT", SWTimage);
+	imshow("connected_components", components_image);
+
 	/// Wait until user exit program by pressing a key
 	waitKey(0);
 
 	return 0;
 }
 
+std::vector<Component> filterComponents(Mat * SWTimage, std::vector<Component> components){
+	std::vector<Component> valid_components;
+	for (std::vector<Component>::iterator it = components.begin(); it != components.end(); it++){
+		it->getStats(SWTimage);
+		if (it->isValid())
+			valid_components.push_back(*it);
+	}
+	return valid_components;
+}
+
+std::vector<std::vector<Component>> connectedPairs(std::vector<Component> valid_components){
+	DisjointSets words(valid_components.size());
+
+	for (int i = 0; i < valid_components.size(); i++){
+		for (int j = 0; j < valid_components.size(); j++){
+			if (valid_components[i].getCenter().x < valid_components[j].getCenter().x){
+				if (valid_components[j].getCenter().y < valid_components[i].maxy){
+					if (valid_components[j].getCenter().y > valid_components[i].miny){
+						if (valid_components[j].isSimilar(valid_components[i])){
+							words.Union(i, j);
+						}
+					}
+				}
+			}
+		}
+	}
+	typedef std::unordered_map<int, std::vector<Component>> componentmap;
+	componentmap map_words;
+
+	for (int i = 0; i < valid_components.size(); i++){
+		int word = words.FindSet(i);
+		map_words[word].push_back(valid_components[i]);
+	}
+
+	std::vector<std::vector<Component>> words_vector;
+	for (componentmap::iterator it = map_words.begin(); it != map_words.end(); it++){
+		if (it->second.size() > 3)
+			words_vector.push_back(it->second);
+	}
+	
+	//for (std::vector<Component>::iterator it = valid_components.begin(); it != valid_components.end(); it++){
+	//	bool matched = false;
+	//	std::vector<Component> canidates = valid_components;
+	//	while (canidates.empty() != 0 && matched != false){
+	//		if (canidates.back().getCenter().x > it->getCenter().x){
+	//			if (canidates.back().getCenter().y < it->maxy && canidates.back().getCenter().y > it->miny){
+	//				if (it->isSimilar(canidates.back())){
+	//					pair_letters.push_back(*it);
+	//					matched = true;
+	//				}	
+	//			}
+	//		}
+	//		canidates.pop_back();
+	//	}/*
+	//	for (std::vector<Component>::iterator it2 = valid_components.begin(); it2 != valid_components.end(); it2++){
+	//		if (it2->getCenter().x > it->getCenter().x){
+	//			if (it2->getCenter().y < it->maxy && it2->getCenter().y > it->miny){
+	//				if (it->isSimilar(*it2))
+	//					pair_letters.push_back(*it);
+	//			}
+	//		}
+	//	}*/
+	//}
+	return words_vector;
+}
+
 Mat SWTransform(Mat* edge_image){
 
 	// accept only char and one channel type matrices
 	CV_Assert(edge_image->depth() != sizeof(uchar));
-	CV_Assert(edge_image->channels() == 1);
+	CV_Assert(edge_image->channels() == 1); 
 
 	int nRows = edge_image->rows;
 	int nCols = edge_image->cols;
-
+	
 	Mat SWTimage = cv::Mat(nRows, nCols, CV_32FC1, cv::Scalar(-1.0));
 	std::vector<std::vector<Point2d>> saved_rays;
 	/*if (I.isContinuous())
@@ -94,7 +263,7 @@ Mat SWTransform(Mat* edge_image){
 	{
 		p = edge_image->ptr<uchar>(i);
 		for (j = 0; j < nCols; ++j)
-		{
+		{	
 			//found a edge
 			if (p[j] == 255){
 				//if (i == 22 && j == 138){
@@ -238,7 +407,7 @@ int strokeMedianFilter(Mat *SWTimage, std::vector<std::vector<Point2d>> *saved_r
 
 int strokeWidth(int j, int i, short g_x, short g_y, Mat *SWTimage,
 	std::vector<std::vector<Point2d>> *saved_rays, Mat *edge_image){
-	/*This function follow the g_x/g_y gradient direction from the D(j,i) point until finding a another edge pint P.
+	/*This function follow the g_x/g_y gradient direction from the D(j,i) point until finding a another edge pint P. 
 	Then both gradient direction are compared. If the dD = -dP +- PI/2 all the pixeles that connect D-P are labeld with the DP length*/
 
 	float step_x, step_y;
@@ -257,7 +426,7 @@ int strokeWidth(int j, int i, short g_x, short g_y, Mat *SWTimage,
 
 		int x = (int)round(sum_x);
 		int y = (int)round(sum_y);
-
+		
 		//pixel out of image
 		if (x < 0 || (x >= (*SWTimage).cols) || y < 0 || (y >= (*SWTimage).rows)) {
 			return 0;
@@ -265,9 +434,9 @@ int strokeWidth(int j, int i, short g_x, short g_y, Mat *SWTimage,
 
 		point.x = x;
 		point.y = y;
-		ray.push_back(point);
-
-		//matched a edge
+		ray.push_back(point); 
+		
+		//matched a edge 
 		if ((*edge_image).at<uchar>(y, x) > 0){
 			short gx_new = -(gradient_x.at<short>(y, x));
 			short gy_new = -(gradient_y.at<short>(y, x));
